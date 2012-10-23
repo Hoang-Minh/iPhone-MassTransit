693e99dc96d86833b34843c9041a234d7c187bbf
KSoto <KReneSoto@gmail.com>
2012-10-23 16:15:12 -0700

    Trying to get pdf view to work

---
+++ Soto.Katie_asgt4/MetrolinkPDFController.h
@@ -0,0 +1,13 @@
+//
+//  MetrolinkPDFController.h
+//  Soto.Katie_asgt4
+//
+//  Created by Lion User on 23/10/2012.
+//  Copyright (c) 2012 Katie Soto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MetrolinkPDFController : UIViewController
+
+@end
---
+++ Soto.Katie_asgt4/MetrolinkPDFController.m
@@ -0,0 +1,35 @@
+//
+//  MetrolinkPDFController.m
+//  Soto.Katie_asgt4
+//
+//  Created by Lion User on 23/10/2012.
+//  Copyright (c) 2012 Katie Soto. All rights reserved.
+//
+
+/*RETRIEVED FROM APPLE*/
+
+#import "MetrolinkPDFController.h"
+#import "MetrolinkPDFScrollView.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation MetrolinkPDFController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	/*
+     Open the PDF document, extract the first page, and pass the page to the PDF scroll view.
+     */
+    
+    NSURL *pdfURL = [[NSBundle mainBundle] URLForResource:@"Metrolink_All_Lines_timetable" withExtension:@"pdf"];
+    
+    CGPDFDocumentRef PDFDocument = CGPDFDocumentCreateWithURL((__bridge CFURLRef)pdfURL);
+    
+    CGPDFPageRef PDFPage = CGPDFDocumentGetPage(PDFDocument, 1);
+    [(MetrolinkPDFScrollView *)self.view setPDFPage:PDFPage];
+    
+    CGPDFDocumentRelease(PDFDocument);
+
+}
+
+@end
